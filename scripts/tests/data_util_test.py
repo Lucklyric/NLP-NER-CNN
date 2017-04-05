5cce862d61fbf7f14d5115cd66522ba939db3f2c
Alvin(Xinyao) Sun <xinyao1@ualberta.ca>
2017-04-04 22:14:05 -0600

    Test data_util save_to_disk function

--- scripts/tests/data_util_test.py
+++ scripts/tests/data_util_test.py
@@ -1,5 +1,8 @@
 from data_util import DataManager
+import data_util
 import numpy as np
-DataInstance = DataManager("../../data/train", "../../data/test", 128)
-batch_input, batch_output, is_epoch_increase = DataInstance.get_batch()
-print (np.shape(batch_input))
+
+# DataInstance = DataManager("../../data/train", "../../data/test", 128)
+# batch_input, batch_output, is_epoch_increase = DataInstance.get_batch()
+# print (np.shape(batch_input))
+data_util.save_to_disk("../../data/train", "../../data/test")
