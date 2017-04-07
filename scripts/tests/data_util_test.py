27a9babf2158392e3026283fc50bac1d83b02a2d
Alvin(Xinyao) Sun <xinyao1@ualberta.ca>
2017-04-06 23:09:15 -0600

    1. Modify data_util_test

--- scripts/tests/data_util_test.py
+++ scripts/tests/data_util_test.py
@@ -3,8 +3,12 @@ import numpy as np
 
 sys.path.append("../")
 import data_util
+from data_util import DataManager
 
 # DataInstance = DataManager("../../data/train", "../../data/test", 128)
 # batch_input, batch_output, is_epoch_increase = DataInstance.get_batch()
 # print (np.shape(batch_input))
-data_util.save_to_disk("../../data/train", "../../data/test")
+# data_util.save_to_disk("../../data/train", "../../data/test")
+DataInstance = DataManager("../../data/train_np.npy", "../../data/test_np.npy", 64)
+sample_input, sample_output = DataInstance.get_one_sample(0)
+print (np.shape(DataInstance._train_data))
